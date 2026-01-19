5e5b28bebf84647e1d698d9f258e5013983c8964
oorskomadsen <o.orskov.madsen@gmail.com>
2026-01-19 12:06:26 +0100

    Inventory system (Start)

--- Player.gd
+++ Player.gd
@@ -8,14 +8,21 @@ const SENS = 0.003
 @onready var head = $Head
 @onready var cam = $Head/Camera3D
 
-func _ready():
+func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	
-func _unhandled_input(event):
+
+func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		head.rotate_y(-event.relative.x * SENS)
 		cam.rotate_x(-event.relative.y * SENS)
-		cam.rotation.x = clamp(cam.rotation.x, deg_to_rad(-80), deg_to_rad(80))
+		cam.rotation.x = clamp(cam.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		var keycode = DisplayServer.keyboard_get_keycode_from_physical(event.physical_keycode)
+		if OS.get_keycode_string(keycode) == "Escape":
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
 	
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -31,10 +38,10 @@ func _physics_process(delta: float) -> void:
 	var input_dir = Input.get_vector("left", "right", "up", "down")
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
+		velocity.x = direction.x * SPEED 
 		velocity.z = direction.z * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED) # is 0
-		velocity.z = move_toward(velocity.z, 0, SPEED) # is 0
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
